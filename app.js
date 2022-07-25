ab8b4ad588b4c77dce38e8420083f7322e003438
Fernando Quezada <fernando@debian.fernando>
2022-07-25 01:37:03 -0400

    actualizacion de imagen de fondo y footer

--- app.js
+++ app.js
@@ -6,7 +6,19 @@ const upload=require("express-fileupload");
 const path=require("path");
 const bodyparser=require("body-parser");
 const nodemailer=require("nodemailer");
-const morgan= require("morgan")
+const morgan= require("morgan");
+
+
+
+
+//configuracion nodemailer
+var transporter=nodemailer.createTransport({
+  service:process.env.MAILSERVICE,
+  auth:{
+    user:process.env.MAILUSER,
+    pass:process.env.MAILPASS
+  }
+});
 
 
 //incializacion
@@ -120,22 +132,100 @@ app.get("/ingresolibros",async function(req,res){
 
   
   app.get ('/ingresoautores', (req, res) => res.render("ingresoautor"));
-  
-  //app.use(bodyParser.unlecoded({ extends: false}));
-  
-  //ingresamos autores
-  /* app.post("/agregarautor", async function(req,res){
-    const consultaId='SELECT COALESCE(MAX("Id),0)+1 AS "Id From "Autores"';
-    let respuestaId;
-    try {
-      respuestaId = await conexion.query(consultaId);
-    } catch (error) {
-      console.log("error consulta:"+error.message);
-      return res.status(500).send("Error al insertar datos:")
-    }
-    const consultaInsert ='Insert into  "Autores" Values'
+  //-------------------------------------------
+//INGRESO AUTORES
+app.get("/ingresoautores",function (req,res) {
+  res.render("ingresoAutor");
+})
+//para obetner texto del body
+app.use(bodyparser.urlencoded({ extended: false })); 
+//registro de autor
+app.post("/agregarautor",async function(req,res){
+  const consultaId='SELECT COALESCE(MAX("Id"),0)+1 AS "Id" FROM "Autores"';
+  let respuetsaId;
+  try {
+    respuetsaId=await conexion.query(consultaId);  
+  } catch (error) {
+    console.log("error consulta:"+error.message)
+    return res.status(500).send("Error al insertar datos");
   }
-   */
+  const consultaInsert='INSERT INTO "Autores" VALUES ($1,$2,$3,$4)'
+  const parametros=[respuetsaId.rows[0].Id,req.body.nombre,req.body.fechaNacimiento,req.body.Nacionalidad];
+  try {
+    await conexion.query(consultaInsert,parametros);
+  } catch (error) {
+    console.log("error consulta insert:"+error.message)
+    return res.status(500).send("Error al insertar datos");
+  }  
+  res.send("Autor agregado");
+})
+
+//-----------------------------------------
+//vista de libros
+app.get("/libro/:id",async function(req,res){
+const id=req.params.id;
+//consutal
+let consulta='select l."Id",l."Nombre" AS "Libro",a."Nombre" as "Autor",l."Edicion", '
+		consulta+= ' l."Paginas",COALESCE(l."Resumen",\'SIN INFORMACIÓN\') AS "Resumen",e."Nombre" as "Editorial",g."Nombre" as "Genero", '
+		consulta+= ' i."Nombre" as "Idioma"'
+ consulta+= ' FROM "Libros" l '
+ consulta+= ' JOIN "Autores" a ON l."IdAutor"=a."Id" '
+ consulta+= ' JOIN "Editorial" e ON l."IdEditorial"=e."Id" '
+ consulta+= ' JOIN "Genero" g ON l."IdGenero"=g."Id" '
+ consulta+= ' JOIN "Idioma" i ON l."IdIdioma"=i."Id"  '
+ consulta+= ' WHERE l."Id"=$1 ';
+
+ const parametros=[id];
+let respuesta;
+ try {
+  respuesta=await conexion.query(consulta,parametros);
+ } catch (error) {
+  console.log("error consulta select:"+error.message)
+  return res.status(500).send("Error al consultar datos");
+ }
+
+ const libro=respuesta.rows[0];
+
+//obtener foto
+const listaArchivos=fs.readdirSync("public/img");  
+let archivo=listaArchivos.filter(a=>
+  a.split(".")[0]==id   
+)
+if(archivo.length==0){ 
+  libro.ruta="../img/noimg.jpg"; 
+}else{
+  libro.ruta="../img/"+archivo[0]; 
+}
+//console.log(libro); 
+res.render("libro",libro)
+
+})
+
+//-----------------------------------------------
+//formulario de contacto - enviar mail
+app.post("/enviarcontacto",function(req,res){
+
+  let mensaje="mensaje desde formulario de contacto\n";
+  mensaje+="de:" + req.body.nombre+"\n";
+  mensaje+="correo:" + req.body.correo+"\n";
+  mensaje+="mensaje:" + req.body.comentario;
+
+  let mail={
+    from: req.body.correo,
+    to: process.env.MAILCONTACTO,
+    subject: 'mensaje formulario contacto',
+    text: mensaje 
+  }
+  transporter.sendMail(mail,function(err,info){
+    if(err){
+      console.log("error enc orreo:" + err.message);
+      res.status(500).send("Error al enviar correo");
+    }else{
+      console.log("correo enviado:" + info.response);
+      res.send("Mensaje enviado");
+    }
+  })
+})
   
 
 module.exports={app}
